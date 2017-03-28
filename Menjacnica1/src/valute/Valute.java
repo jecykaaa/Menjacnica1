59252e24c38994466014398f80f5ccdbb163dd71
jecykaaa96 <jecykaaa96@gmail.com>
2017-03-28 23:06:22 +0200

    Dodata logicka kontrola u sve set metode iz klase Valute

--- Menjacnica1/src/valute/Valute.java
+++ Menjacnica1/src/valute/Valute.java
@@ -10,31 +10,46 @@ public class Valute {
 	public String getNaziv() {
 		return naziv;
 	}
-	public void setNaziv(String naziv) {
+	public void setNaziv(String naziv) throws Exception {
+		if(naziv.isEmpty() || naziv == null) {
+			throw new Exception();
+		}
 		this.naziv = naziv;
 	}
 	public String getSkraceniNaziv() {
 		return skraceniNaziv;
 	}
-	public void setSkraceniNaziv(String skraceniNaziv) {
+	public void setSkraceniNaziv(String skraceniNaziv) throws Exception {
+		if(skraceniNaziv.isEmpty() || skraceniNaziv == null) {
+			throw new Exception();
+		}
 		this.skraceniNaziv = skraceniNaziv;
 	}
 	public double getProdajniKurs() {
 		return prodajniKurs;
 	}
-	public void setProdajniKurs(double prodajniKurs) {
+	public void setProdajniKurs(double prodajniKurs) throws Exception {
+		if(prodajniKurs <= 0) {
+			throw new Exception();
+		}
 		this.prodajniKurs = prodajniKurs;
 	}
 	public double getKupovniKurs() {
 		return kupovniKurs;
 	}
-	public void setKupovniKurs(double kupovniKurs) {
+	public void setKupovniKurs(double kupovniKurs) throws Exception {
+		if(kupovniKurs <= 0) {
+			throw new Exception();
+		}
 		this.kupovniKurs = kupovniKurs;
 	}
 	public double getSrednjiKurs() {
 		return srednjiKurs;
 	}
-	public void setSrednjiKurs(double srednjiKurs) {
+	public void setSrednjiKurs(double srednjiKurs) throws Exception {
+		if(srednjiKurs <= 0) {
+			throw new Exception();
+		}
 		this.srednjiKurs = srednjiKurs;
 	}
 	
