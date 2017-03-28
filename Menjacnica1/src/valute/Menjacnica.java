4d9e455dec6f5948de021c626870da28067ea118
jecykaaa <jecykaaa96@gmail.com>
2017-03-28 23:59:50 +0200

    Dodata klasa Menjacnica koja implementira interfejs MenjacnicaInterfejs

---
+++ Menjacnica1/src/valute/Menjacnica.java
@@ -0,0 +1,25 @@
+package valute;
+
+import java.util.GregorianCalendar;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKursValuteZaOdredjeniDan(double kurs, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKursValuteZaOdredjeniDan(double kurs, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double pronadjiIVratiKursValuteZaOdredjeniDan(double kurs, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
